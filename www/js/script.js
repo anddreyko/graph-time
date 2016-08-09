6a38e060299ba7d2842d2196fb129946ad7cddff
anddreyko <anddreyko@mail.ru>
2016-08-09 09:05:03 +0300

    add list edges

--- www/js/script.js
+++ www/js/script.js
@@ -1,29 +1,21 @@
 $(document).ready(function(){
 
     var cnv = $('canvas')
-      , cx = cnv[0].getContext('2d');
+  , cx = cnv[0].getContext('2d')
+  , list = [ [150, 50, 20], [250, 150, 40] ];
     cnv[0].width = cnv.width();
     cnv[0].height = cnv.height();
 
-    cx.fillStyle = 'red';
+    cx.fillStyle = 'rgb(200, 200, 200, 0.5)';
 
     cx.beginPath();
-    cx.arc(150, 50, 40, 0, 2 * Math.PI);
-
-    cx.closePath();
-    cx.arc(450, 50, 10, 0, 2 * Math.PI);
-    cx.closePath();
+    list.forEach(function(e){
+        //cx.lineTo(e[0], e[1]);
+        cx.arc(e[0], e[1], e[2], 0, 2 * Math.PI);
+        //cx.moveTo(e[0], e[1]);
+        //cx.closePath();
+    });
     cx.fill();
-
-    cx.moveTo(50, 10);
-    cx.lineTo(10, 70);
-    cx.lineTo(90, 70);
-    cx.closePath();
-
-    cx.moveTo(150, 110);
-    cx.lineTo(110, 170);
-    cx.lineTo(190, 70);
-    cx.closePath();
-    cx.stroke();
+    //cx.stroke();
 
 });
