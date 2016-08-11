8235a9714b64c19c54b6c3ab3d9d38d12d9033d4
anddreyko <anddreyko@mail.ru>
2016-08-11 22:32:48 +0300

    fix

--- socket/index.js
+++ socket/index.js
@@ -2,13 +2,99 @@ var log = require('../lib/#log')(module)
   , sec = require('../lib/#sec')
   , conf = require('../conf')
   , async = require('async')
-  , HttpError = require('../error').HttpError;
+  , HttpError = require('../error').HttpError
+  , d
+  , s1
+  , m1
+  , h1
+  , d1
+  , f1
+  , listVertex
+  , listRibs;
 
 module.exports = function(server) {
     var io = require('socket.io').listen(server);
-    io.set('origins', conf.get('uri')+':*');
+    io.set('origins', '*:*');
     io.sockets.on('connection', function(s) {
-        
+        setInterval(function(){
+            d = new Date();
+            s1 = d.getSeconds() + 0 + d.getMilliseconds() / 1000;
+            m1 = d.getMinutes() + 0 + s1 / 60;
+            h1 = d.getHours() + 0 + m1 / 60;
+            d1 = d.getDate() + 0 + h1 / 24;
+            f1 = d.getMonth() + 0 + d1 / 30;
+            listVertex = [
+                [
+                    s1 / 10
+                  , 9
+                  , 10
+                ], [
+                    s1 % 10
+                  , 9
+                  , 10
+                ] , [
+                    m1 / 10
+                  , 8
+                  , 12
+                ], [
+                    m1 % 10
+                  , 8
+                  , 12
+                ] , [
+                    h1 / 10
+                  , 7
+                  , 15
+                ], [
+                    h1 % 10
+                  , 7
+                  , 15
+                ] , [
+                    d1 / 10
+                  , 6
+                  , 17
+                ], [
+                    d1 % 10
+                  , 6
+                  , 17
+                ] , [
+                    f1 / 10
+                  , 5
+                  , 20
+                ], [
+                    f1 % 10
+                  , 5
+                  , 20
+                ]
+            ];
+            listRibs = [
+                [
+                    listVertex[0]
+                  , listVertex[2]
+                ], [
+                    listVertex[2]
+                  , listVertex[4]
+                ], [
+                    listVertex[4]
+                  , listVertex[6]
+                ], [
+                    listVertex[6]
+                  , listVertex[8]
+                ], [
+                    listVertex[1]
+                  , listVertex[3]
+                ], [
+                    listVertex[3]
+                  , listVertex[5]
+                ], [
+                    listVertex[5]
+                  , listVertex[7]
+                ], [
+                    listVertex[7]
+                  , listVertex[9]
+                ]
+            ]
+            s.emit('draw', listRibs);
+        }, 34);
     });
     return io;
 }
--- www/js/script.js
+++ www/js/script.js
@@ -6,7 +6,8 @@ $(document).ready(function(){
   , heightScreen = $(window).height()
   , kX1 = widthScreen / 21
   , kX2 = widthScreen / 2
-  , kY = heightScreen / 10;
+  , kY = heightScreen / 10
+  , socket = io.connect('', { 'reconnect': false });
   
     cnv[0].width = cnv.width();
     cnv[0].height = cnv.height();
@@ -16,88 +17,11 @@ $(document).ready(function(){
     cx.strokeStyle = '#363636';
 
     cx.lineWidth = 1;
-
-    (function f() {
+    
+    socket.on('draw', function(listRibs){
         cx.clearRect(0, 0, widthScreen, heightScreen);
         cx.beginPath();
-        var d = new Date()
-      , s1 = d.getSeconds() + 0 + d.getMilliseconds() / 1000
-      , m1 = d.getMinutes() + 0 + s1 / 60
-      , h1 = d.getHours() + 0 + m1 / 60
-      , d1 = d.getDate() + 0 + h1 / 24
-      , f1 = d.getMonth() + 0 + d1 / 30
-      , listVertex = [
-            [
-                s1 / 10
-              , 9
-              , 10
-            ], [
-                s1 % 10
-              , 9
-              , 10
-            ] , [
-                m1 / 10
-              , 8
-              , 12
-            ], [
-                m1 % 10
-              , 8
-              , 12
-            ] , [
-                h1 / 10
-              , 7
-              , 15
-            ], [
-                h1 % 10
-              , 7
-              , 15
-            ] , [
-                d1 / 10
-              , 6
-              , 17
-            ], [
-                d1 % 10
-              , 6
-              , 17
-            ] , [
-                f1 / 10
-              , 5
-              , 20
-            ], [
-                f1 % 10
-              , 5
-              , 20
-            ]
-        ]
-      , listRibs = [
-            [
-                listVertex[0]
-              , listVertex[2]
-            ], [
-                listVertex[2]
-              , listVertex[4]
-            ], [
-                listVertex[4]
-              , listVertex[6]
-            ], [
-                listVertex[6]
-              , listVertex[8]
-            ], [
-                listVertex[1]
-              , listVertex[3]
-            ], [
-                listVertex[3]
-              , listVertex[5]
-            ], [
-                listVertex[5]
-              , listVertex[7]
-            ], [
-                listVertex[7]
-              , listVertex[9]
-            ]
-        ]
-      , listRibsLength = $(listRibs).length / 2;
-//console.log((d.getMilliseconds()) / 1000 + s1);
+        var listRibsLength = $(listRibs).length / 2;
         listRibs.forEach(function(e, i){
             var kX = kX1;
             cx.moveTo(e[0][0]*kX +  ( i < listRibsLength ? 0 : kX2), e[0][1]*kY);
@@ -111,7 +35,7 @@ $(document).ready(function(){
         cx.stroke();
         cx.fill();
         cx.closePath();
-        setTimeout(f, 24);
-    })();
-
+    })
+    
+    
 });
