3d0dc6eec2e32f4f4dfb6c049b3f6a231633a8be
anddreyko <anddreyko@mail.ru>
2016-08-11 22:53:47 +0300

    fix

--- www/js/script.js
+++ www/js/script.js
@@ -1,5 +1,4 @@
 $(document).ready(function(){
-
     var cnv = $('canvas')
   , cx = cnv[0].getContext('2d')
   , socket = io.connect('', { 'reconnect': false })
@@ -7,9 +6,59 @@ $(document).ready(function(){
   , heightScreen = $(window).height()
   , kX1 = widthScreen / 21
   , kX2 = widthScreen / 2
-  , kY = heightScreen / 10;
+  , kY = heightScreen / 10
+  , COLOR = '#363636'
+  , BACKGROUND = '#2a2a2a';
     cnv[0].width = widthScreen;
     cnv[0].height = heightScreen;
+
+    cnv.css({'background': BACKGROUND});
+    cx.fillStyle = COLOR;
+    cx.strokeStyle = COLOR;
+    cx.lineWidth = 1;
+
+    socket.on('draw', function(listRibs){
+        cx.clearRect(0, 0, widthScreen, heightScreen);
+        cx.beginPath();
+        var listRibsLength = $(listRibs).length / 2;
+        listRibs.forEach(function(e, i){
+            var kX = ( i < listRibsLength ? 0 : kX2);
+            cx.moveTo(
+                e[0][0]*kX1 + kX
+              , e[0][1]*kX1
+            );
+            cx.lineTo(
+                e[1][0]*kX1 + kX
+              , e[1][1]*kX1
+            );
+
+            cx.moveTo(
+                e[0][0]*kX1 + kX
+              , e[0][1]*kX1
+            );
+            cx.arc(
+                e[0][0]*kX1 + kX
+              , e[0][1]*kX1
+              , e[0][2]
+              , 0
+              , 2 * Math.PI
+            );
+            cx.moveTo(
+                e[1][0]*kX1 + kX
+              , e[1][1]*kX1
+            );
+            cx.arc(
+                e[1][0]*kX1 + kX
+              , e[1][1]*kX1
+              , e[1][2]
+              , 0
+              , 2 * Math.PI
+            );
+        });
+        cx.stroke();
+        cx.fill();
+        cx.closePath();
+    })
 
     $(window).resize(function(){
         widthScreen = $(window).width();
@@ -19,33 +68,7 @@ $(document).ready(function(){
         kY = heightScreen / 10;
         cnv[0].width = widthScreen;
         cnv[0].height = heightScreen;
+        cx.fillStyle = COLOR;
+        cx.strokeStyle = COLOR;
     });
-
-    cnv.css({'background': '#2a2a2a'});
-
-    cx.fillStyle = '#363636';
-    cx.strokeStyle = '#363636';
-
-    cx.lineWidth = 1;
-    
-    socket.on('draw', function(listRibs){
-        cx.clearRect(0, 0, widthScreen, heightScreen);
-        cx.beginPath();
-        var listRibsLength = $(listRibs).length / 2;
-        listRibs.forEach(function(e, i){
-            var kX = kX1;
-            cx.moveTo(e[0][0]*kX +  ( i < listRibsLength ? 0 : kX2), e[0][1]*kY);
-            cx.lineTo(e[1][0]*kX +  ( i < listRibsLength ? 0 : kX2), e[1][1]*kY);
-
-            cx.moveTo(e[0][0]*kX +  ( i < listRibsLength ? 0 : kX2), e[0][1]*kY);
-            cx.arc(e[0][0]*kX +  ( i < listRibsLength ? 0 : kX2), e[0][1]*kY, e[0][2], 0, 2 * Math.PI);
-            cx.moveTo(e[1][0]*kX +  ( i < listRibsLength ? 0 : kX2), e[1][1]*kY);
-            cx.arc(e[1][0]*kX +  ( i < listRibsLength ? 0 : kX2), e[1][1]*kY, e[1][2], 0, 2 * Math.PI);
-        });
-        cx.stroke();
-        cx.fill();
-        cx.closePath();
-    })
-    
-    
 });
